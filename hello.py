3f8c858d44b36bf07e88d0af9913a4d409ecb13a
Gustavo Miranda <gustavocataldomiranda@gmail.com>
2024-10-16 18:28:07 -0300

    update

--- hello.py
+++ hello.py
@@ -3,44 +3,21 @@ import pandas as pd
 import os
 
 # Nome do arquivo CSV onde os dados serão armazenados
-data_file = "survey_data.csv"
+data_file = "compras.csv"
 
-# Opções de estados
-estados = [
-    "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará",
-    "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão",
-    "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará",
-    "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
-    "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima",
-    "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
+
+mercados = [
+    "Zona Sul", "Guanabara", "Amazon", "Pão de Açúcar", "Ultra", "FarmaLife"
 ]
-
-# Opções de áreas de atuação
-areas_atuacao = ["Analista de Dados", "Cientista de Dados", "Engenheiro de Dados"]
-
-# Opções de bibliotecas
-bibliotecas = [
-    "Pandas", "Pydantic", "scikit-learn", "Git", "Pandera", "streamlit",
-    "postgres", "databricks", "AWS", "Azure", "airflow", "dbt",
-    "Pyspark", "Polars", "Kafka", "Duckdb", "PowerBI", "Excel", "Tableau", "storm"
-]
-
-# Opções de horas codando
-horas_codando = ["Menos de 5", "5-10", "10-20", "Mais de 20"]
-
-# Opções de conforto com dados
-conforto_dados = ["Desconfortável", "Neutro", "Confortável", "Muito Confortável"]
 
 # Criação do formulário
-with st.form("dados_enquete"):
-    estado = st.selectbox("Estado", estados)
-    area_atuacao = st.selectbox("Área de Atuação", areas_atuacao)
-    bibliotecas_selecionadas = st.multiselect("Bibliotecas e ferramentas mais utilizadas", bibliotecas)
-    horas_codando = st.selectbox("Horas Codando ao longo da semana", horas_codando)
-    conforto_dados = st.selectbox("Conforto ao programar e trabalhar com dados", conforto_dados)
-    experiencia_python = st.slider("Experiência de Python", 0, 10)
-    experiencia_sql = st.slider("Experiência de SQL", 0, 10)
-    experiencia_cloud = st.slider("Experiência em Cloud", 0, 10)
+with st.form("dados_enquete", clear_on_submit=True):
+    # produto = st.selectbox("Produto", estados)
+    produto = st.text_input("Produto")
+    preco = st.number_input("Preço")
+    mercado = st.selectbox("Mercado", mercados, )
+    promocao = st.checkbox("Promoção")
+    data = st.date_input("Data da Compra", "today", format="DD/MM/YYYY")
 
     # Botão para submeter o formulário
     submit_button = st.form_submit_button("Enviar")
@@ -48,14 +25,11 @@ with st.form("dados_enquete"):
 # Se o botão foi clicado, salvar os dados no DataFrame e no CSV
 if submit_button:
     novo_dado = {
-        "Estado": estado,
-        "Bibliotecas e ferramentas": ", ".join(bibliotecas_selecionadas),
-        "Área de Atuação": area_atuacao,
-        "Horas de Estudo": horas_codando,
-        "Conforto com Dados": conforto_dados,
-        "Experiência de Python": experiencia_python,
-        "Experiência de SQL": experiencia_sql,
-        "Experiência em Cloud": experiencia_cloud,
+        "Produto": produto,
+        "Preço": preco,
+        "Mercado": mercado,
+        "Promoção": promocao,
+        "Data": data
     }
     new_data = pd.DataFrame([novo_dado])
 
