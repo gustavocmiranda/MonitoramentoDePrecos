b3633d51907c62ea1dd2665bbfca69bbe3108c2e
Gustavo Miranda <56138255+gustavocmiranda@users.noreply.github.com>
2024-11-28 00:34:14 -0300

    api no backend

--- app/backend/crud.py
+++ app/backend/crud.py
@@ -11,3 +11,46 @@ def get_one_buy(db: Session, buy_id: int):
 
 def get_last_buy(db:Session):
     return db.query(BuyModel).order_by(BuyModel.id.desc()).first()
+
+def create_buy(db: Session, buy: BuyCreate):
+    db_buy = BuyModel(**buy.model_dump())
+    db.add(db_buy)
+
+    db.commit()
+
+    # db.refresh(db_buy)
+
+    return db_buy
+
+def delete_buy(db: Session, buy_id: int):
+    db_buy = db.query(BuyModel).filter(BuyModel.id == buy_id).first()
+    db.delete(db_buy)
+
+    db.commit()
+    return db_buy
+
+def update_buy(db: Session, buy_id: int, buy: BuyUpdate):
+    db_buy = db.query(BuyModel).filter(BuyModel.id == buy_id).first()
+
+    if buy is None:
+        return None
+    
+    if buy.produto is not None:
+        db_buy.produto = buy.produto
+
+    if buy.preco is not None:
+        db_buy.preco = buy.preco
+
+    if buy.local_de_compra is not None:
+        db_buy.local_de_compra = buy.local_de_compra
+    
+    if buy.promocao is not None:
+        db_buy.promocao = buy.promocao
+    
+    if buy.data_de_compra is not None:
+        db_buy.data_de_compra = buy.data_de_compra
+
+    db.commit()
+    db.refresh(db_buy)
+
+    return db_buy
--- app/backend/router.py
+++ app/backend/router.py
@@ -3,5 +3,55 @@ from sqlalchemy.orm import Session
 from database.database import SessionLocal, get_db
 from schemas.schemas import BuyCreate, BuyResponse, BuyUpdate
 from typing import List
+from crud import (
+    create_buy,
+    get_all_buys,
+    get_last_buy,
+    get_one_buy,
+    update_buy,
+    delete_buy
+)
 
 router = APIRouter()
+
+
+@router.get(path='/buys/', response_model=List[BuyResponse])
+def read_all_buys(db: Session = Depends(get_db)):
+    buys = get_all_buys(db)
+    return buys
+
+@router.get(path='/buys/{buy_id}', response_model=BuyResponse)
+def read_buy(buy_id: int, db: Session = Depends(get_db)):
+    db_buy = get_one_buy(db, buy_id)
+
+    if db_buy is None:
+        raise HTTPException(status_code=404, detail='Compra não encontrada.')
+    
+    return db_buy
+
+@router.get(path='/last/', response_model=BuyResponse)
+def read_last_buy(db: Session = Depends(get_db)):
+    db_buy = get_last_buy(db)
+    return db_buy
+
+@router.post(path='/buys/', response_model=BuyResponse)
+def post_buy(buy: BuyCreate, db: Session = Depends(get_db)):
+    return create_buy(db, buy)
+
+@router.delete(path='/buys/{buy_id}', response_model=BuyResponse)
+def delete_one_buy(buy_id: int, db: Session = Depends(get_db)):
+    db_buy = delete_buy(db, buy_id)
+
+    if db_buy is None:
+        raise HTTPException(status_code=404, detail='Compra não encontrada.')
+
+    return db_buy
+
+@router.put('/buys/{buy_id}', response_model=BuyResponse)
+def put_buy(buy_id: int, buy: BuyUpdate, db: Session = Depends(get_db)):
+    db_buy = update_buy(db, buy_id, buy)
+
+    if db_buy is None:
+            raise HTTPException(status_code=404, detail='Compra não encontrada.')
+
+    return db_buy
