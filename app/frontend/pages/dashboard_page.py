4c6c09a20a79a22d5b03dd63aa2b8bc9fb052e44
Gustavo Miranda <56138255+gustavocmiranda@users.noreply.github.com>
2024-12-07 23:07:57 -0300

    inicio dashboard

--- app/frontend/pages/dashboard_page.py
+++ app/frontend/pages/dashboard_page.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pandas as pd
+import requests
 
 st.sidebar.markdown('Dashboard')
-st.markdown('Dashboard')
+st.title('Dashboard')
+
+response = requests.get("http://backend:8000/buys/")
+if response.status_code == 200:
+    data = pd.DataFrame(response.json())
+
+
+grouped_data = data['local_de_compra'].value_counts().reset_index()
+grouped_data.columns = ['local_de_compra', 'numero_de_compras']
+
+# Exibir o gráfico de barras
+st.bar_chart(grouped_data.set_index('local_de_compra'))
